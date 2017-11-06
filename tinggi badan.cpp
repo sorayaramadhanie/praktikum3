0f2b7598c02874bd4a766ed272e1f807778b34d7
sorayaramadhaniabdi <sorayaramadhani617@gmail.com>
2017-11-06 14:21:40 +0700

    tinggi badan

--- tinggi badan.cpp
+++ tinggi badan.cpp
@@ -5,5 +5,20 @@ using namespace std;
 
 int main() 
 {
-  
+    float b;
+    int t;
+    
+    cout << "masukkan tinggi badan: "; cin >>t;
+    cout << "masukkan berat badan: " ;
+    cin >> b;
+    
+    if (b<t/2.5)
+       cout << "Underweight" << endl;
+            else if (t/2.3 < b)
+                 cout << "Overweight" << endl;
+            else if (t/2.5 <= b <= t/2.3)
+           		 cout << "Normal" << endl;
+                 
+                 
+                 return 0;
     }
