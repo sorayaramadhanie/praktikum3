6ed759fa4d20b398bbfdbcbadf376797d20a7b64
sorayaramadhaniabdi <sorayaramadhani617@gmail.com>
2017-11-06 14:00:24 +0700

    jenisbilangan

--- menentukanJenisBilangan.cpp
+++ menentukanJenisBilangan.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main(){
+	int p;
+	cout<<"Input Bilangan : ";cin>>p;
+	cout<<""<<endl;
+	if (p < 0)
+	{
+		cout<<"anda input adalah bilangan negatif"<<endl;
+	}
+	
+	else if (p>0)
+	{
+		cout<<"anda input adalah bilangan positif"<<endl;
+	}
+	else if (p==0)
+	
+		cout<<"anda input adalah bilangan nol "<<endl;
 
+	return 0;
 }
