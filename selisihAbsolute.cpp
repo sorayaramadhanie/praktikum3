da85b876ff1af9a03b32be704c965c93bcb05596
sorayaramadhaniabdi <sorayaramadhani617@gmail.com>
2017-11-06 14:05:08 +0700

    selisih absolut

--- selisihAbsolute.cpp
+++ selisihAbsolute.cpp
@@ -3,5 +3,20 @@
 using namespace std;
 
 int main(){
+	int k,l,selisih;
+	cout<<" Input Bilangan x : "; cin>>k;
+	cout<<" Input Bilangan y : "; cin>>l;
 
+if (k>l){
+	selisih=k-l;
+	
+	cout<<"Selisih Nilai X dan Y yaitu "<<selisih;
+}
+
+else if (l>k){
+	selisih=l-k;
+
+	cout<<"Selisih Nilai Y dan X yaitu "<<selisih;
+}
+	return 0;
 }
